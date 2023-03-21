43142aa9751269c12448c74dce4e3c1510b222ab
PedroHgds <phgds741@hotmail.com>
2023-03-21 10:49:24 -0300

    nomeIdade

---
+++ NomeIdade.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class NomeIdade {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner (System.in);
+        String nome;
+        int idade; 
+
+        while(true){
+        System.out.println("Nome: ");
+            nome = scan.next();
+        if (nome.equals(0)) break;
+
+        System.out.println("Idade: ");
+            idade = scan.nextInt();
+        }
+        System.out.println("Continua aqui ...");
+    }
+}
